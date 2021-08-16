2a1dd0c3c20924d922a8e8d4e9552708f7543e14
olamilekan-adeleke <olamilekanadeleke123@gmail.com>
2021-08-16 22:38:10 +0100

    implemented faker to populate backend with users

--- lib/cores/utils/config.dart
+++ lib/cores/utils/config.dart
@@ -1,4 +1,5 @@
 import 'package:get_storage/get_storage.dart' as storage;
+import 'package:kod_chat/cores/faker/generate_random_user_faker.dart';
 import 'package:kod_chat/cores/utils/locator.dart';
 import 'firebase_messaging_utils.dart';
 
@@ -12,5 +13,11 @@ class Config {
   //init all bindings for get x
   static void setUpBindings() {
     setUpLocator();
+  }
+
+
+  //init all faker set up
+  static Future<void> fakerSetup() async {
+    await GenerateRandomUserFaker.start();
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -9,5 +9,6 @@ Future<void> main() async {
   await Firebase.initializeApp();
   await Config.setUpHiveLocalDB();
   Config.setUpBindings();
+  // await Config.fakerSetup();
   runApp(KodChatApp());
 }
